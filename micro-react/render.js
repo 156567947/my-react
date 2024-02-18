0d125d79f644eae468d46b65fdba0226a2c3c10d
huangjinyuan <156567947@qq.com>
2024-02-19 00:41:08 +0800

    ✨ feat: Concurrent Mode

--- micro-react/render.js
+++ micro-react/render.js
@@ -23,4 +23,24 @@ function render(element, container) {
   //追加到父节点
   container.appendChild(dom);
 }
+
+let nextUnitOfWork = null;
+function workLoop(deadLine) {
+  //应该退出
+  let shouldTield = false;
+  //有工作 且 不应该退出
+  while (nextUnitOfWork && !shouldTield) {
+    //做工作
+    nextUnitOfWork = performUnitOfWork(nextUnitOfWork);
+    //看看有没有足够时间
+    shouldTield = deadLine.timeRemaining() < 1;
+  }
+  //没有做够时间，请求下一次浏览器空闲时继续执行
+  requestIdleCallback(workLoop);
+}
+//第一次请求
+requestIdleCallback(workLoop);
+
+function performUnitOfWork(work) {}
+
 export default render;
